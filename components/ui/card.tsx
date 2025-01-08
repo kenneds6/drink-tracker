290975aca0389239fe760b64b20146df82b911df
kenneds6 <kenneds6@mail.uc.edu>
2025-01-08 13:25:29 -0500

    Fix TypeScript errors in card component

--- components/ui/card.tsx
+++ components/ui/card.tsx
@@ -2,34 +2,46 @@
 
 import * as React from "react"
 
-interface CardProps extends React.HTMLAttributes<HTMLDivElement> {}
-interface CardHeaderProps extends React.HTMLAttributes<HTMLDivElement> {}
-interface CardTitleProps extends React.HTMLAttributes<HTMLDivElement> {}
-interface CardContentProps extends React.HTMLAttributes<HTMLDivElement> {}
+// Remove empty interfaces and use type instead
+type CardProps = React.HTMLAttributes<HTMLDivElement>
+type CardHeaderProps = React.HTMLAttributes<HTMLDivElement>
+type CardTitleProps = React.HTMLAttributes<HTMLHeadingElement>
+type CardContentProps = React.HTMLAttributes<HTMLDivElement>
 
-const Card: React.FC<CardProps> = ({ className = '', ...props }) => (
+const Card = React.forwardRef<HTMLDivElement, CardProps>(({ className = '', ...props }, ref) => (
   <div
+    ref={ref}
     className={`rounded-lg border bg-white shadow-sm ${className}`}
     {...props}
   />
-)
+))
+Card.displayName = "Card"
 
-const CardHeader: React.FC<CardHeaderProps> = ({ className = '', ...props }) => (
+const CardHeader = React.forwardRef<HTMLDivElement, CardHeaderProps>(({ className = '', ...props }, ref) => (
   <div
+    ref={ref}
     className={`flex flex-col space-y-1.5 p-6 ${className}`}
     {...props}
   />
-)
+))
+CardHeader.displayName = "CardHeader"
 
-const CardTitle: React.FC<CardTitleProps> = ({ className = '', ...props }) => (
+const CardTitle = React.forwardRef<HTMLHeadingElement, CardTitleProps>(({ className = '', ...props }, ref) => (
   <h3
+    ref={ref}
     className={`text-2xl font-semibold leading-none tracking-tight ${className}`}
     {...props}
   />
-)
+))
+CardTitle.displayName = "CardTitle"
 
-const CardContent: React.FC<CardContentProps> = ({ className = '', ...props }) => (
-  <div className={`p-6 pt-0 ${className}`} {...props} />
-)
+const CardContent = React.forwardRef<HTMLDivElement, CardContentProps>(({ className = '', ...props }, ref) => (
+  <div 
+    ref={ref}
+    className={`p-6 pt-0 ${className}`} 
+    {...props} 
+  />
+))
+CardContent.displayName = "CardContent"
 
 export { Card, CardHeader, CardTitle, CardContent }
